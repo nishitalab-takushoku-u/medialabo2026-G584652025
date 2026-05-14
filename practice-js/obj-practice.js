31204dfdc6993dd84cf8b5d66c5884a4d8ea4449
Ryota Tonouchi <258485@st.takushoku-u.ac.jp>
2026-05-14 14:02:17 +0900

    obj-practice.jsを編集

--- practice-js/obj-practice.js
+++ practice-js/obj-practice.js
@@ -15,4 +15,11 @@ let gakka = [
 ];
 
 //////////////// ここから下にプログラムを書きたそう!
+console.log(campus.address);
+for (let r of campus.buildingD) {
+	console.log(r);
+}
+for (let g of gakka) {
+	console.log(g.name);
+}
 
