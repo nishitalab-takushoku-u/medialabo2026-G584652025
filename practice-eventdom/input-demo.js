f2f9077dba8af8b404c98edecfa16e7527cc1f98
Ryota Tonouchi <258485@st.takushoku-u.ac.jp>
2026-05-14 15:13:31 +0900

    input-demo.jsを編集

--- practice-eventdom/input-demo.js
+++ practice-eventdom/input-demo.js
@@ -1,6 +1,8 @@
 b = document.querySelector('button#print');
 b.addEventListener('click', greeting);
 function greeting() {
+    console.log('こんにちは');
+    
     let i = document.querySelector('input[name="shimei"]');
     aisatu = 'こんにちは' + i.value + 'さん';
     let p = document.querySelector('p#message');
